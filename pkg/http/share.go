9205fc6555321944b001b5c7ec7cece71e2fde1d
itchenyi <i@liusha.me>
2018-09-26 17:14:28 +0800

    support file share

---
+++ pkg/http/share.go
@@ -0,0 +1,36 @@
+package http
+
+import (
+	"net/http"
+
+	"time"
+
+	"strconv"
+
+	"github.com/labstack/echo"
+	"github.com/srelab/ossproxy/pkg/sftp"
+)
+
+type ShareHandler struct{}
+
+func (handler ShareHandler) Init(g *echo.Group) {
+	g.GET("/*", handler.Get)
+}
+
+func (ShareHandler) Get(ctx echo.Context) error {
+	prefix := ctx.Param("*")
+	expire, err := strconv.Atoi(ctx.QueryParam("expire"))
+
+	if err != nil {
+		expire = 10
+	}
+
+	if prefix == "" {
+		prefix = "/"
+	}
+
+	return SuccessResponse(ctx, http.StatusOK, &BaseResult{
+		Result:  sftp.Bucket.SignedURL(prefix, time.Now().Add(time.Duration(expire)*time.Minute)),
+		Success: true,
+	})
+}
